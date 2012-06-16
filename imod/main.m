ce1869541d6800c2e5c499084f42b5e4593e1a08
Mod Technologies LLC <modtechnologiesllc@gmail.com>
2012-06-15 21:57:34 -0400

    Hello from ModTech

--- imod/main.m
+++ imod/main.m
@@ -1,4 +1,4 @@
-//iMod Installer Application
+//iMod Installer Application:
 //Copyright © 2012 Modtech (written by phyrrus9 <phyrrus9@gmail.com>)
 //this is free software, and can be used however it needs or wants to be 
 //(yes, it has a mind of its own). good luck, dont ask me for help im too lazy.
