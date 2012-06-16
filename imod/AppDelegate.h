6391e0311b941e137820555a0241c2d9dc177233
Ethan Laur <phyrrus9@gmail.com>
2012-06-16 18:54:23 -0400

    Fixed the stupid copyright…. (@5urd sucks)

--- imod/AppDelegate.h
+++ imod/AppDelegate.h
@@ -1,5 +1,5 @@
 //iMod Installer Application
-//Copyright © 2012 Modtech (written by phyrrus9 <phyrrus9@gmail.com>)
+//Copyright © 2012 Mod Technologies (written by phyrrus9 <phyrrus9@gmail.com>)
 //this is free software, and can be used however it needs or wants to be 
 //(yes, it has a mind of its own). good luck, dont ask me for help im too lazy.
 #import <UIKit/UIKit.h>
